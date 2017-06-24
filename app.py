db851f964ff4b0966b5d3e07bf1b828bd06ed3cc
jieyuxu <jieyu.stuy@gmail.com>
2017-06-24 15:29:57 -0400

    login register pages

--- app.py
+++ app.py
@@ -16,6 +16,18 @@ app.config.update(dict( # Make sure the secret key is set for use of the session
 def home():
     return render_template("home.html")
 
+@app.route("/pauthenticate/")
+def donorlogin():
+    return render_template("authenticate.html", donor=True)
+
+@app.route("/hauthenticate/")
+def shelterlogin():
+    return render_template("authenticate.html")
+
+@app.route("/login/")
+def login():
+    return
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
