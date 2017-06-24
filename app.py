0d0d5729aa199fba5913ed32f542821757e3ab39
jieyuxu <jieyu.stuy@gmail.com>
2017-06-24 17:21:55 -0400

    login works

--- app.py
+++ app.py
@@ -6,22 +6,32 @@ from httplib2 import Http # The http library to issue REST calls to the oauth ap
 import json # Json library to handle replies
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 @app.route("/authenticate/")
-def donorlogin():
+def auth():
     return render_template("authenticate.html", donor=True)
 
-@app.route("/login/")
+@app.route("/login/", methods=["POST"])
 def login():
     if request.form["login"]:
         email = request.form["email"]
         passw = hashlib.sha512(request.form["password"]).hexdigest()
-        if authenticate_user(email, passw):
-            session["donor"] = email
+        print email
+        print passw
+        result, msg = db_manager.authenticate_user(email, passw)
+        print result
+        print msg
+        if result == True:
+            session["user"] = email
+            print session["user"]
+            return redirect(url_for("dashboard"))
+        else:
+            return render_template("authenticate.html", msg = msg)
     else:
         return redirect(url_for("home"))
 
@@ -32,20 +42,40 @@ def login():
 #  "password" : "a38jg38vno83oour"
 #  "type" : "donor" // "director"
 # }
-@app.route("/register/")
+@app.route("/register/", methods=["POST"])
 def register():
     if request.form["register"]:
+        if hashlib.sha512(request.form["password"]).hexdigest() != hashlib.sha512(request.form["cpassword"]).hexdigest():
+            return redirect(url_for("auth"), msg = "Passwords don't match.")
         user = {}
         user.update({"email" : request.form["email"]})
         user.update({"first_name" : request.form["first"]})
         user.update({"last_name" : request.form["last"]})
-        user.update({"password" : request.form["password"]})
+        user.update({"password" : hashlib.sha512(request.form["password"]).hexdigest()})
         user.update({"type" : request.form["type"]})
         db_manager.add_user(user)
-        return url_for("dashboard")
+        return redirect(url_for("auth"))
     else:
-        return redirect(url_for("home"))
+        return redirect(url_for("auth"), msg = "Register failed.")
 
+@app.route("/logout/")
+def logout():
+    if session["user"]:
+        session.pop("user")
+    return redirect(url_for("home"))
+
+@app.route("/dashboard/")
+def dashboard():
+    print db_manager.get_user(session["user"])
+    print db_manager.get_user(session["user"])["type"]
+    if db_manager.get_user(session["user"])["type"] == None:
+        return redirect(url_for("auth"))
+    if db_manager.get_user(session["user"])["type"] == "donor":
+        return render_template("dashboard.html", donor=True)
+    elif db_manager.get_user(session["user"])["type"] == "director":
+        return render_template("dashboard.html", director = True)
+    else:
+        return redirect(url_for("auth"))
 if __name__ == '__main__':
     app.debug = True
     app.run()
