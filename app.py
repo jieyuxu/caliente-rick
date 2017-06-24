27b9c0eb096fb243643c39320cb81dadf53dea15
jieyuxu <jieyu.stuy@gmail.com>
2017-06-24 14:17:10 -0400

    app.py routes begin

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import os, sys, random, csv
 from flask import Flask, render_template, url_for, request, redirect, session, make_response
-from utils import auth
-from utils import db_manager
 import hashlib
 #oauth imports and stuff
 from oauth2client.client import flow_from_clientsecrets, OAuth2Credentials # OAuth library, import the function and class that this uses
