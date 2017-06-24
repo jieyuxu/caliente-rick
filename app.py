15c3db9cce12012a7a0ffe850b59ddb0fbd9d0e0
jieyuxu <jieyu.stuy@gmail.com>
2017-06-24 16:20:13 -0400

    bug

--- app.py
+++ app.py
@@ -1,37 +1,27 @@
-import os, sys, random
 from flask import Flask, render_template, url_for, request, redirect, session, make_response
 from utils import db_manager
 import hashlib
-#oauth imports and stuff
+import os, sys, random
 from httplib2 import Http # The http library to issue REST calls to the oauth api
 import json # Json library to handle replies
 
 app = Flask(__name__)
-app.secret_key = os.urandom(32)
-app.config.update(dict( # Make sure the secret key is set for use of the session variable
-    SECRET_KEY = 'secret'
-    ))
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/pauthenticate/")
+@app.route("/authenticate/")
 def donorlogin():
     return render_template("authenticate.html", donor=True)
 
 @app.route("/login/")
 def login():
-    if request.form["dlogin"]:
+    if request.form["login"]:
         email = request.form["email"]
         passw = hashlib.sha512(request.form["password"]).hexdigest()
         if authenticate_user(email, passw):
             session["donor"] = email
-    elif request.form["slogin"]:
-        email = request.form["email"]
-        passw = hashlib.sha512(request.form["password"]).hexdigest()
-        if authenticate_user(email, passw):
-            session["shelter"] = email
     else:
         return redirect(url_for("home"))
 
