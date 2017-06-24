3b35dce7d9007e2fd0d20aa4fb284f60c2039839
jieyuxu <jieyu.stuy@gmail.com>
2017-06-24 16:07:24 -0400

    routes for login

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import os, sys, random, csv
+import os, sys, random
 from flask import Flask, render_template, url_for, request, redirect, session, make_response
+from utils import db_manager
 import hashlib
 #oauth imports and stuff
 from httplib2 import Http # The http library to issue REST calls to the oauth api
-
 import json # Json library to handle replies
 
 app = Flask(__name__)
@@ -20,13 +20,41 @@ def home():
 def donorlogin():
     return render_template("authenticate.html", donor=True)
 
-@app.route("/hauthenticate/")
-def shelterlogin():
-    return render_template("authenticate.html")
-
 @app.route("/login/")
 def login():
-    return
+    if request.form["dlogin"]:
+        email = request.form["email"]
+        passw = hashlib.sha512(request.form["password"]).hexdigest()
+        if authenticate_user(email, passw):
+            session["donor"] = email
+    elif request.form["slogin"]:
+        email = request.form["email"]
+        passw = hashlib.sha512(request.form["password"]).hexdigest()
+        if authenticate_user(email, passw):
+            session["shelter"] = email
+    else:
+        return redirect(url_for("home"))
+
+# user_info: dictionary
+# {"email" : "example@gmail.com",
+#  "first_name" : "Ex",
+#  "last_name" : "Ample"}
+#  "password" : "a38jg38vno83oour"
+#  "type" : "donor" // "director"
+# }
+@app.route("/register/")
+def register():
+    if request.form["register"]:
+        user = {}
+        user.update({"email" : request.form["email"]})
+        user.update({"first_name" : request.form["first"]})
+        user.update({"last_name" : request.form["last"]})
+        user.update({"password" : request.form["password"]})
+        user.update({"type" : request.form["type"]})
+        db_manager.add_user(user)
+        return url_for("dashboard")
+    else:
+        return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.debug = True
